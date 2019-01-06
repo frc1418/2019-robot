6ba488136435d655e2ff3ac938e895726b580d66
JosephCarpenter <carp5@rcn.com>
2019-01-06 13:33:32 -0500

    Copy framework for main robot code

---
+++ robot/robot.py
@@ -0,0 +1,82 @@
+import magicbot
+import wpilib
+import wpilib.drive
+
+from wpilib.buttons import JoystickButton
+from robotpy_ext.control.button_debouncer import ButtonDebouncer
+from components import drive
+from magicbot import tunable
+
+from robotpy_ext.common_drivers import navx
+
+class Robot(magicbot.MagicRobot):
+    drive = drive.Drive
+
+    def createObjects(self):
+        """
+        Initialize robot components.
+        """
+        # Joysticks
+        self.joystick_left = wpilib.Joystick(0)
+        self.joystick_right = wpilib.Joystick(1)
+        self.joystick_alt = wpilib.Joystick(2)
+
+        # Determine button functions after robot is designed
+
+        # NavX (purple board on top of the RoboRIO)
+        self.navx = navx.AHRS.create_spi()
+        self.navx.reset()
+
+        # Utility
+        self.ds = wpilib.DriverStation.getInstance()
+        self.timer = wpilib.Timer()
+        self.pdp = wpilib.PowerDistributionPanel(0)
+        self.compressor = wpilib.Compressor()
+
+        # Camera server
+        wpilib.CameraServer.launch('camera/camera.py:main')
+
+    def robotPeriodic(self):
+        """
+        Executed periodically regardless of mode.
+        """
+        self.time = int(self.timer.getMatchTime())
+        self.voltage = self.pdp.getVoltage()
+        self.rotation = self.navx.getAngle() % 360
+
+    def autonomous(self):
+        """
+        Prepare for and start autonomous mode.
+        """
+
+        # Call autonomous
+        super().autonomous()
+
+    def disabledInit(self):
+        """
+        Executed once right away when robot is disabled.
+        """
+        # Reset Gyro to 0
+        self.navx.reset()
+
+    def disabledPeriodic(self):
+        """
+        Executed periodically while robot is disabled.
+        Useful for testing.
+        """
+        pass
+
+    def teleopInit(self):
+        """
+        Executed when teleoperated mode begins.
+        """
+        self.compressor.start()
+
+    def teleopPeriodic(self):
+        """
+        Executed periodically while robot is in teleoperated mode.
+        """
+
+
+    if __name__ == '__main__':
+    wpilib.run(Robot)
