988fcec5c3472ad3fdcdf16b9c5db79eaee17899
JosephCarpenter <carp5@rcn.com>
2019-01-07 16:07:34 -0500

    Stop ignoring E701

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 set -e
 
 # E501 line too long
-# E701 multiple statements on one line
-# Currently, E701 takes issue with some type annotations. (PyCQA/pycodestyle#682)
-pycodestyle . --ignore=E501,E701
+pycodestyle . --ignore=E501
 # Re-add the line below after basic project structure is set up
 # python3 robot/robot.py test
