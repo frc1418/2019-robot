9cbea022830931549486aab3a4290412561e3627
ErikBoesen <me@erikboesen.com>
2019-01-20 16:13:37 -0500

    Replace rio scripts with single rio.sh

--- rio-download.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-robotpy-installer download-robotpy
-robotpy-installer download-opkg $(< roborio-opkgs.txt)
-robotpy-installer download-pip $(< roborio-requirements.txt)
--- rio-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-robotpy-installer install-robotpy
-robotpy-installer install-opkg $(< roborio-opkgs.txt)
-robotpy-installer install-pip $(< roborio-requirements.txt)
