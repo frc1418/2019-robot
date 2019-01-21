68b58d22d74142070506b143afb3a82ee97b023f
JosephCarpenter <carp5@rcn.com>
2019-01-21 13:31:51 -0500

    Create framework for lift mechanism

---
+++ robot/components/lift.py
@@ -0,0 +1,40 @@
+import wpilib
+from magicbot import will_reset_to
+from magicbot import tunable
+
+
+class Lift:
+    """
+    Operate robot object-lifting mechanism.
+    """
+    lift: wpilib.Victor
+
+    lift_speed = will_reset_to(0)
+    motion_constant = tunable(0.6)
+
+    def move(self, speed: float):
+        """
+        Set the motor speed of the lift.
+        :param speed: The requested speed, between -1 and 1.
+        """
+        self.lift_speed = speed
+
+    def up(self):
+        """
+        Move lift upward.
+        Used when controlling arm through buttons.
+        """
+        self.lift_speed = 1 * self.motion_constant
+
+    def down(self):
+        """
+        Move lift downward.
+        Used when controlling arm through buttons.
+        """
+        self.lift_speed = -1 * self.motion_constant
+
+    def execute(self):
+        """
+        Run elevator motors.
+        """
+        self.lift.set(-self.lift_speed)
