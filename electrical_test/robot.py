9c1b25367dc08e257eb34a997fef9490a3568138
AndrewLester <alester220@gmail.com>
2019-02-02 16:30:04 -0500

    Add encoders to electrical test

--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -3,17 +3,29 @@
 import magicbot
 import wpilib
 import wpilib.drive
+import math
 
 from ctre.wpi_talonsrx import WPI_TalonSRX
 
 
 class TestRobot(magicbot.MagicRobot):
+    ENCODER_PULSE_PER_REV = 1024
+    WHEEL_DIAMETER = 0.5
     def createObjects(self):
         """
         Initialize testbench components.
         """
+        encoder_constant = (
+            (1 / self.ENCODER_PULSE_PER_REV) * self.WHEEL_DIAMETER * math.pi
+        )
+
+        self.l_encoder = wpilib.Encoder(2, 3)
+        self.l_encoder.setDistancePerPulse(encoder_constant)
+        self.r_encoder = wpilib.Encoder(0, 1)
+        self.r_encoder.setDistancePerPulse(encoder_constant)
         self.joystick = wpilib.Joystick(0)
         self.lift_talon = WPI_TalonSRX(40)
+
 
     def teleopPeriodic(self):
         """
