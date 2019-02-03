8e7a817700447e575c64c33e61b1fbace2f1c4ef
ErikBoesen <me@erikboesen.com>
2019-02-03 14:13:39 -0500

    Use TalonSRX controllers for intake

--- robot/robot.py
+++ robot/robot.py
@@ -55,14 +55,11 @@ class Robot(magicbot.MagicRobot):
 
         # Lift motors
         self.lift_motors = wpilib.SpeedControllerGroup(WPI_TalonSRX(30),
-                                                       WPI_TalonSRX(35),
-                                                       WPI_TalonSRX(40),
-                                                       WPI_TalonSRX(45))
-
+                                                       WPI_TalonSRX(35))
         self.hatch_solenoid = wpilib.DoubleSolenoid(2, 3)
-        self.left_cargo_intake_motor = wpilib.Spark(0)
+        self.left_cargo_intake_motor = WPI_TalonSRX(40)
         self.left_cargo_intake_motor.setInverted(True)
-        self.right_cargo_intake_motor = wpilib.Spark(1)
+        self.right_cargo_intake_motor = WPI_TalonSRX(45)
         self.cargo_intake_motors = wpilib.SpeedControllerGroup(self.left_cargo_intake_motor,
                                                                self.right_cargo_intake_motor)
 
