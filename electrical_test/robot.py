dedd8a7f1365fd054fc3b902ba3ddf5b37f9c45c
ErikBoesen <me@erikboesen.com>
2019-02-06 09:29:28 -0500

    Remove lift code from electrical_test

--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -11,17 +11,12 @@ class TestRobot(magicbot.MagicRobot):
         Initialize testbench components.
         """
         self.joystick = wpilib.Joystick(0)
-        self.lift_motors = [wpilib.Victor(i) for i in range(4, 8)]
-        for motor in self.lift_motors:
-            motor.setDeadband(0.2)
 
     def teleopPeriodic(self):
         """
         Spin all motors at full speed.
         """
-        for motor in self.lift_motors:
-            # Correcting for odd joystick
-            motor.set(-self.joystick.getY() * 2 - 1)
+        pass
 
 
 if __name__ == '__main__':
