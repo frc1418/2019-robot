6380aebd33dc9d87400d919a57ce5a9701c80378
ErikBoesen <me@erikboesen.com>
2019-02-06 22:45:55 -0500

    Add PID logic support to lift

--- robot/components/lift.py
+++ robot/components/lift.py
@@ -17,7 +17,19 @@ class Lift:
     lift_forward = tunable(False)
     motion_constant = tunable(0.6)
 
+    target_kp = tunable(0.99)
+    target_ki = tunable(0.00)
+    target_kd = tunable(0.00)
+    target_tolerance = tunable(100)
+    previous_error = 0
+
     ENCODER_TICKS_PER_REVOLUTION = 12345  # FIXME: NOT THE REAL VALUE!
+
+    def on_enable(self):
+        """
+        Prepare component for operation.
+        """
+        self.i_err = 0
 
     @property
     def current_ticks(self):
@@ -25,6 +37,24 @@ class Lift:
         Get current position of lift in encoder ticks.
         """
         return self.lift_motor.getSelectedSensorOutput()
+
+    def target(self, target_ticks) -> bool:
+        """
+        Adjusts the lift using PID to a given number of ticks.
+
+        :param target_ticks: Position in ticks desired.
+        :returns: Whether robot has reached requested position
+        """
+        tick_error = target_ticks - self.current_ticks
+        if abs(tick_error) > self.target_tolerance:
+            self.i_err += angle_error
+            self.rot = self.target_kp * angle_error + self.target_ki * self.i_err + self.target_kd * (self.previous_error - angle_error) / 0.020
+            self.rot = max(min(self.target_max_rot, self.rot), -self.target_max_rot)
+
+            self.previous_error = angle_error
+            return False
+        self.i_err = 0
+        return True
 
     def move(self, speed: float):
         """
