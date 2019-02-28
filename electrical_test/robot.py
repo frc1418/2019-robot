cdf74c5bcb261cac54fd3cb57f9ba0ab65bb4b03
ErikBoesen <me@erikboesen.com>
2019-02-27 19:30:07 -0500

    Add simple tankdrive for electrical_test

--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -13,27 +13,17 @@ class TestRobot(magicbot.MagicRobot):
     WHEEL_DIAMETER = 0.5
 
     def createObjects(self):
-        """
-        Initialize testbench components.
-        """
-        self.joystick = self.Joystick(2)
-        encoder_constant = (
-            (1 / self.ENCODER_PULSE_PER_REV) * self.WHEEL_DIAMETER * math.pi
-        )
-
-        self.l_encoder = wpilib.Encoder(2, 3)
-        self.l_encoder.setDistancePerPulse(encoder_constant)
-        self.r_encoder = wpilib.Encoder(0, 1)
-        self.r_encoder.setDistancePerPulse(encoder_constant)
-        self.joystick = wpilib.Joystick(0)
-        self.lift_talon = WPI_TalonSRX(40)
+        self.joystick_left = wpilib.Joystick(0)
+        self.joystick_right = wpilib.Joystick(1)
+        self.lf_motor = WPI_TalonSRX(10)
+        self.lr_motor = WPI_TalonSRX(15)
+        self.rf_motor = WPI_TalonSRX(20)
+        self.rr_motor = WPI_TalonSRX(25)
+        # Drivetrain
+        self.train = wpilib.drive.DifferentialDrive(wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor), wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor))
 
     def teleopPeriodic(self):
-        """
-        Spin all motors at full speed.
-        """
-        print(self.joystick.getRawButton(12))
-
+        self.train.arcadeDrive(-self.joystick_left.getY(), self.joystick_right.getX())
 
 if __name__ == '__main__':
     wpilib.run(TestRobot)
