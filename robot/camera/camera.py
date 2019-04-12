2eaaeaa7570d8cf77eb656aee88d093345bc4bba
ErikBoesen <me@erikboesen.com>
2019-04-12 15:51:39 -0400

    Delete fluff from camera

--- robot/camera/camera.py
+++ robot/camera/camera.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from cscore import CameraServer, UsbCamera
-from networktables import NetworkTables
 
 
 def main():
@@ -10,21 +9,8 @@ def main():
     usb0 = cs.startAutomaticCapture(dev=0)
     usb1 = cs.startAutomaticCapture(dev=1)
 
-    camera0 = NetworkTables.getTable('CameraPublisher/USB Camera 0')
-    camera1 = NetworkTables.getTable('CameraPublisher/USB Camera 1')
-
     cs.waitForever()
 
 
 if __name__ == "__main__":
-
-    # To see messages from networktables, you must setup logging
-    import logging
-
-    logging.basicConfig(level=logging.DEBUG)
-
-    # You should uncomment these to connect to the RoboRIO
-    # import networktables
-    # networktables.initialize(server='10.xx.xx.2')
-
     main()
